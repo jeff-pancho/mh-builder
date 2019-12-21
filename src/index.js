5ba42676ae1f509a988ef6d2c4bfde19dda95fc6
Jeff Pancho <jepoypancho@hotmail.com>
2019-12-20 18:01:26 -0800

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,75 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class Square extends React.Component {
+    render () {
+        return (
+            <div className="square">
+                1
+            </div>
+        );
+    }
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+class Grid extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            butt: "ass"
+        }
+    }
+
+    renderSquare(i) {
+        return (
+            <Square 
+                key={i}
+            />
+        )
+    }
+
+    render() {
+        let squares = [];
+        const width = this.props.width;
+        const height = this.props.height;
+
+        for (let y = 0; y < height; y++) {
+            let column = [];
+            for (let x = 0; x < width; x++) {
+                column.push(this.renderSquare(x));
+            }
+            squares.push(
+                <div 
+                    key={y}
+                    className="grid-row"
+                >
+                    {column}
+                </div>
+            );
+        }
+
+        return (
+            <div>
+                {squares}
+            </div>
+        );
+    }
+}
+
+class Builder extends React.Component {
+    render() {
+        return (
+            <div>
+                <Grid 
+                    width={10}
+                    height={10}
+                />
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(
+    <Builder />,
+    document.getElementById('root')
+);
