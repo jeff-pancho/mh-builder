689af5bb0e26325c839c11f56b16d80bb50cbde7
Jeff Pancho <jepoypancho@hotmail.com>
2019-12-30 00:00:49 -0800

    basic grid

--- src/components/Builder.js
+++ src/components/Builder.js
@@ -1,11 +1,61 @@
 import React from "react";
 
-class Builder extends React.Component {
-    render() {
+// 46x46 original size
+const WIDTH = 20;
+const HEIGHT = 20;
+
+class Grid extends React.Component {
+    renderSquare(row, column) {
         return (
-            <div id="builder"></div>
+            <td
+                key={`${row},${column}`}
+                className="square"
+            >
+            </td>
         );
     }
+
+    render() {
+        let squareGrid = [];
+        let {width, height} = this.props;
+        for (let row = 0; row < height; row++) {
+            let columns = []
+            for (let column = 0; column < width; column++) {
+                columns.push(this.renderSquare(row, column));
+            }
+            squareGrid.push(<tr>{columns}</tr>);
+        }
+
+        return (
+            <table cellSpacing="0" cellPading="0">
+                <thead>{squareGrid}</thead>
+            </table>
+        );
+    }
+}
+
+class Builder extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            grid: generateEmptyGrid(WIDTH, HEIGHT)
+        };
+    }
+
+    render() {
+        return (
+            <div id="builder">
+                <Grid width={WIDTH} height={HEIGHT}/>
+            </div>
+        );
+    }
+}
+
+const generateEmptyGrid = (width, height) => {
+    let grid = Array(height).fill().map(() => {
+        Array(width).fill("#FFFFFF");
+    });
 }
 
 export default Builder;
--- src/index.js
+++ src/index.js
@@ -3,10 +3,6 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import Builder from "./components/Builder.js"
 
-/*
- * width:   46
- * height:  46
- */
 ReactDOM.render(
     <Builder />,
     document.getElementById('root')
