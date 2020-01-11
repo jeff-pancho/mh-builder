28a3ad5038dbdb6726c2f86918bcc26348f40cdf
Jeff Pancho <jepoypancho@hotmail.com>
2020-01-10 23:05:46 -0800

    don't re-render placed buildings

--- src/components/Building.js
+++ src/components/Building.js
@@ -1,6 +1,31 @@
 import React from "react";
 
-const Building = ({row, column, width, height, colour}) => {
+class Building extends React.Component {
+    shouldComponentUpdate(nextProps) {
+        let { row, column } = this.props;
+        return nextProps.row !== row || nextProps.column !== column;
+    }
+
+    render() {
+        let { row, column, width, height, colour } = this.props;
+        let style = {
+            backgroundColor: colour,
+            gridColumn: column + 1 + " / span " + width,
+            gridRow: row + 1 + " / span " + height
+        };
+
+        return (
+            <div
+                className="building"
+                style={style}
+            >
+            </div>
+        );
+    }
+}
+
+/*
+const Building = ({ row, column, width, height, colour }) => {
     let style = {
         backgroundColor: colour,
         gridColumn: column + 1 + " / span " + width,
@@ -15,6 +40,6 @@ const Building = ({row, column, width, height, colour}) => {
         </div>
     );
 }
-
+*/
 
 export default Building;
